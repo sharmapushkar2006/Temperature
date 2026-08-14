b18b4bab0d5f45563debf66ad210497eeb3e68b9
Pushkar Sharma <spushkar2205@gmail.com>
2026-08-14 23:01:18 +0530

    Discussed about the design of the website.

--- Temperature/src/App.jsx
+++ Temperature/src/App.jsx
@@ -5,8 +5,8 @@ function App() {
   return (
    <>
     <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex'>
-      <h1 className='!text-[100px] text-red-500'>0</h1>
-      {/* <label>&deg; C</label> */}
+      <h1 className='!text-[100px] text-yellow-500 font-medium'>0</h1>
+      <label>&deg; C</label>
     </div>
    </>
   )
