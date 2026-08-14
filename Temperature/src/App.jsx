0e7a53bf0be77ed1f518675318fc1f6106ecf695
Pushkar Sharma <spushkar2205@gmail.com>
2026-08-14 21:10:46 +0530

    first commit

---
+++ Temperature/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect} from 'react'
+import './App.css'
+
+function App() {
+  return (
+   <>
+    <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex'>
+      <h1 className='!text-[100px] text-red-500'>0</h1>
+      {/* <label>&deg; C</label> */}
+    </div>
+   </>
+  )
+}
+
+export default App
