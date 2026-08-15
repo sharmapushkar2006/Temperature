8b02efd87d1be1212ba3823835e0778c772ffab5
Pushkar Sharma <spushkar2205@gmail.com>
2026-08-15 13:51:17 +0530

    Added function to check for browser support for geolocation.

--- Temperature/src/App.jsx
+++ Temperature/src/App.jsx
@@ -2,16 +2,11 @@ import { useState, useEffect} from 'react'
 import './App.css'
 
 function App() {
-  const url
-  const [temp,settemp]=useState("0");
-  useEffect(()=>{
-    if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(success, error);
-   } else {
-    alert("Geolocation is not supported by this browser.");
-   }
-
-  },[temp])
+  const [temp,settemp]=useState("Locating..");
+  var url;
+  function lin(x,y){
+    url=`https://api.open-meteo.com/v1/forecast?latitude=${x}&longitude=${y}&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max&hourly=temperature_2m&current=temperature_2m,is_day&timezone=auto&forecast_days=1`
+  }
 
   async function success(position) {
     lin(position.coords.latitude,position.coords.longitude);
@@ -23,11 +18,24 @@ function App() {
     uv.innerHTML="UV Index: "+data.daily.uv_index_max[0];
 
 }
+
+
+
+useEffect(()=>{
+    if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success);
+    console.log("Location fetch success! contacting API for data")
+   } else {
+     console.log("Location not found")
+     settemp('Not found')
+   }
+
+  },[temp])
   return (
    <>
     <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 item-center flex flex-col gap-6'>
       <div className='flex items-start text-center'>
-        <h1 className='!text-[100px] text-yellow-500 font-medium'>0</h1>
+        <h1 className='!text-[100px] text-yellow-500 font-medium'>{temp}</h1>
         <label>&deg; C</label>
       </div>
       <div className='flex flex-col text-center'>
